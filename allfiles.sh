e3e88688dae10f3cdd5357cd62d4e2bcc32c3794
myhobby2 <49870972+myhobby2@users.noreply.github.com>
2019-05-23 14:36:28 +0200

    Update and rename allfiles to allfiles.sh

---
+++ allfiles.sh
@@ -0,0 +1,4 @@
+wget --no-check-certificate https://github.com/myhobby2/Settings/blob/master/userbouquet.stream_usa.tv
+wget --no-check-certificate ttps://github.com/myhobby2/Settings/blob/master/userbouquet.france.tv
+wget --no-check-certificate https://github.com/myhobby2/Settings/blob/master/userbouquet.stream_monaco.tv
+wget --no-check-certificate https://github.com/myhobby2/Settings/blob/master/userbouquet.stream_frankrijk.tv
