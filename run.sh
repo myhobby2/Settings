7fe6ef968e31ca317b1d26e431a9748bb94acedb
myhobby2 <49870972+myhobby2@users.noreply.github.com>
2019-05-24 21:22:11 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 # download these files
+wget --no-check-certificate https://github.com/myhobby2/Settings/raw/master/bouquets.tv
 wget --no-check-certificate https://github.com/myhobby2/Settings/raw/master/userbouquet.france.tv
+wget --no-check-certificate https://github.com/myhobby2/Settings/raw/master/userbouquet.stream_frankrijk.tv
+wget --no-check-certificate https://github.com/myhobby2/Settings/raw/master/userbouquet.stream_monaco.tv
+wget --no-check-certificate https://github.com/myhobby2/Settings/raw/master/userbouquet.stream_usa.tv
