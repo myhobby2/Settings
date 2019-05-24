046831ecfbde317c15d022cc4e65cf13f5296ef0
myhobby2 <49870972+myhobby2@users.noreply.github.com>
2019-05-24 21:46:43 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ wget --no-check-certificate https://github.com/myhobby2/Settings/raw/master/user
 wget --no-check-certificate https://github.com/myhobby2/Settings/raw/master/userbouquet.stream_frankrijk.tv
 wget --no-check-certificate https://github.com/myhobby2/Settings/raw/master/userbouquet.stream_monaco.tv
 wget --no-check-certificate https://github.com/myhobby2/Settings/raw/master/userbouquet.stream_usa.tv
+wget --no-check-certificate https://github.com/myhobby2/Settings/raw/master/userbouquet.docu.tv
